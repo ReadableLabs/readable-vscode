03cb562fda8138db465e92c96beb41c950c4f608
Nevin1901 <nevinpuri1901@gmail.com>
2021-11-09 01:03:16 +0100

    comment

--- src/TextGenerator.ts
+++ src/TextGenerator.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 export default class TextGenerator {
   private _baseUrl = "http://127.0.0.1:8000";
   private _completionUrl = this._baseUrl + "/complete/";
+
+  private _languages = ["docstring", "summary", "in_line"];
   constructor() {}
 
   private async makeApiRequest(route: string): Promise<any> {
@@ -18,6 +20,12 @@ export default class TextGenerator {
   }
 
   private formatText(language: string) {}
+
+  public async generate(code: string, language: string, type: string) {
+    if (this._languages.indexOf(type) === -1) {
+      throw new Error("Error: invalid comment type"); // TODO: check language
+    }
+  }
 
   public async generateSummary(text: string) {
     const data = await this.makeApiRequest(this._completionUrl);
