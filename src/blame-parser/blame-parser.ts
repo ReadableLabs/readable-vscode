ed1e76914c5fa1166cf130bde0e35eefc402a8f1
Nevin <nevinpuri1901@gmail.com>
2022-03-11 14:40:53 +0100

    got diff parsing working

--- src/blame-parser/blame-parser.ts
+++ src/blame-parser/blame-parser.ts
@@ -2,7 +2,7 @@ import IParsedBlame from "./types/IParsedBlame";
 
 export const parseBlame = (blame: string[]): IParsedBlame[] => {
   let totalLines = blame.length / 13; // 52, so 4 times
-  let totalBlames = new Array<IParsedBlame>(totalLines);
+  let totalBlames: IParsedBlame[] = [];
   for (let i = 0; i < totalLines; i++) {
     let offset = i * 13; // so 13 * 0 = 0 = 1st line. 13 * 1 = 13th line = 2nd blame
     // for (let k = offset; k < offset + 13; k++) {
@@ -14,26 +14,26 @@ export const parseBlame = (blame: string[]): IParsedBlame[] => {
     let originalLine = parseInt(splitLine[1]);
     let finalLine = parseInt(splitLine[2]);
     let lineGroup = splitLine.length > 3 ? parseInt(splitLine[3]) : undefined;
-    totalBlames[i].line = {
+    let blameLine = {
       commit: commitId,
       originalLine: originalLine,
       finalLine: finalLine,
       commitLine: lineGroup,
     };
     // just push to the blame instead of this shit or make an IParsedBlame object and push that
-    let author = blame[offset + 1];
-    let authorMail = blame[offset + 2];
-    let authorTime = parseInt(blame[offset + 3]);
-    let authorTz = blame[offset + 4];
-    let commiter = blame[offset + 5];
-    let commiterMail = blame[offset + 6];
-    let commiterTime = blame[offset + 7];
-    let commiterTz = blame[offset + 8];
-    let summary = blame[offset + 9];
-    let boundary = blame[offset + 10];
-    let fileName = blame[offset + 11];
-    let line = blame[offset + 12];
-    totalBlames[i].info = {
+    let author = getStringValue(blame[offset + 1]);
+    let authorMail = getStringValue(blame[offset + 2]);
+    let authorTime = getStringValue(blame[offset + 3]);
+    let authorTz = getStringValue(blame[offset + 4]);
+    let commiter = getStringValue(blame[offset + 5]);
+    let commiterMail = getStringValue(blame[offset + 6]);
+    let commiterTime = getIntValue(blame[offset + 7]);
+    let commiterTz = getStringValue(blame[offset + 8]);
+    let summary = getStringValue(blame[offset + 9]);
+    let boundary = getStringValue(blame[offset + 10]);
+    let fileName = getStringValue(blame[offset + 11]);
+    let line = getStringValue(blame[offset + 12]);
+    let blameInfo = {
       author,
       authorMail,
       authorTime,
@@ -47,9 +47,38 @@ export const parseBlame = (blame: string[]): IParsedBlame[] => {
       fileName,
       line,
     };
+    totalBlames.push({
+      info: blameInfo,
+      line: blameLine,
+    });
     //   }
     // might be 14 lines
     // }
   }
   return totalBlames;
+};
+
+const getStringValue = (line: string): string | undefined => {
+  let splitLine = line.split(" ");
+  if (splitLine.length < 2) {
+    return;
+  }
+  return splitLine[1];
+};
+
+const getIntValue = (line: string) => {
+  let value = getStringValue(line);
+  if (!value) {
+    return;
+  }
+  return parseInt(value);
+};
+
+const getDateValue = (line: string) => {
+  let value = getStringValue(line);
+  if (!value) {
+    return;
+  }
+
+  return Date.parse(value);
 };
--- src/blame-parser/types/IInfoChunk.ts
+++ src/blame-parser/types/IInfoChunk.ts
@@ -1,14 +1,14 @@
 export default interface IInfoChunk {
-  author: string;
-  authorMail: string;
-  authorTime: number;
-  authorTz: string;
-  commiter: string;
-  commiterMail: string;
-  commiterTime: string;
-  commiterTz: string;
-  summary: string;
-  boundary: string;
-  fileName: string;
-  line: string;
+  author?: string;
+  authorMail?: string;
+  authorTime?: string;
+  authorTz?: string;
+  commiter?: string;
+  commiterMail?: string;
+  commiterTime?: number;
+  commiterTz?: string;
+  summary?: string;
+  boundary?: string;
+  fileName?: string;
+  line?: string;
 }
