b6e99760ccc42472490fc1c457f4a07f551e7e06
Nevin1901 <nevinpuri1901@gmail.com>
2022-03-19 21:27:59 +0100

    made alt tab work

--- src/commentSync/newCommenySyncProvider.ts
+++ src/commentSync/newCommenySyncProvider.ts
@@ -9,6 +9,19 @@ import { needsUpdating } from "./sync";
 
 export default class CommentSync {
   constructor() {
+    vscode.window.onDidChangeActiveTextEditor(async (e) => {
+      if (!e) {
+        return;
+      }
+      if (!vscode.workspace.workspaceFolders) {
+        return;
+      }
+      const folder = vscode.workspace.workspaceFolders[0].uri.fsPath;
+      const file = path.relative(folder, e.document.uri.fsPath);
+      let ranges = await getOutOfSyncComments(folder, file, e.document);
+      updateDecorations(ranges);
+      // get each out of sync line, add it to  total dict with line and range, and then for each of the changed functions, check the params
+    });
     vscode.workspace.onDidSaveTextDocument(async (e) => {
       if (!vscode.workspace.workspaceFolders) {
         return;
@@ -17,28 +30,8 @@ export default class CommentSync {
       const file = path.relative(folder, e.uri.fsPath);
       console.log(file);
       await sync(folder);
-      let blameText = await blame(folder, file);
-      const symbols = await getFunctions();
-      const document = getDocumentTextFromEditor(e);
-      let updatingRanges: vscode.Range[] = [];
-      for (let symbol of symbols) {
-        let commentRange = getCommentRange(
-          symbol.range.start.line - 2,
-          document.split("\n")
-        );
-        if (!commentRange) {
-          continue;
-        }
-        if (needsUpdating(symbol, commentRange, blameText)) {
-          console.log(
-            `${symbol.name} with comment range ${commentRange.start.line} - ${commentRange.end.line} needs updating`
-          );
-          updatingRanges.push(commentRange);
-        }
-      }
-      updateDecorations(updatingRanges);
-      console.log(blameText);
-      console.log("done");
+      let ranges = await getOutOfSyncComments(folder, file, e);
+      updateDecorations(ranges);
     });
   }
 }
@@ -68,4 +61,34 @@ const getFunctions = async () => {
     }
   }
   return fullSymbols;
+};
+
+const getOutOfSyncComments = async (
+  folder: string,
+  file: string,
+  editor: vscode.TextDocument
+) => {
+  let blameText = await blame(folder, file);
+  const symbols = await getFunctions();
+  const document = getDocumentTextFromEditor(editor);
+  let updatingRanges: vscode.Range[] = [];
+  for (let symbol of symbols) {
+    let commentRange = getCommentRange(
+      symbol.range.start.line - 2,
+      document.split("\n")
+    );
+    if (!commentRange) {
+      continue;
+    }
+    if (needsUpdating(symbol, commentRange, blameText)) {
+      console.log(
+        `${symbol.name} with comment range ${commentRange.start.line} - ${commentRange.end.line} needs updating`
+      );
+      updatingRanges.push(commentRange);
+    }
+  }
+  return updatingRanges;
+  updateDecorations(updatingRanges);
+  console.log(blameText);
+  console.log("done");
 };
