9b54d4c37a191c1b160fe652e31c7c597261efda
Nevin1901 <nevinpuri1901@gmail.com>
2022-08-18 20:16:44 +0200

    split into process and download manager

---
+++ src/resync/downloadManager.ts
@@ -0,0 +1,69 @@
+import * as fs from "fs";
+import * as path from "path";
+import * as https from "https";
+import ReadableLogger from "../Logger";
+import { DownloadState } from "./types";
+
+export abstract class DownloadManager {
+  public static downloading: boolean = false;
+  public static dir: string;
+  public static bin: string;
+
+  public static setDir(directory: string) {
+    this.dir = directory;
+    this.bin = path.join(this.dir, "resync");
+  }
+
+  public static isDownloaded() {
+    return fs.existsSync(this.bin);
+  }
+
+  public static download() {
+    if (this.isDownloaded() || DownloadManager.downloading) {
+      return DownloadState.Ok;
+    }
+
+    return new Promise<DownloadState>((resolve, reject) => {
+      try {
+        ReadableLogger.log("Downloading resync");
+        this.downloading = true;
+        let platform = `resync_${process.platform}_${process.arch}`;
+        let downloadUrl = `https://resync.readable.workers.dev/${platform}`;
+
+        try {
+          fs.mkdirSync(this.dir);
+        } catch (err) {}
+
+        https.get(downloadUrl, { rejectUnauthorized: false }, (res) => {
+          let file = fs.createWriteStream(this.bin);
+          res.pipe(file);
+
+          file.on("finish", () => {
+            ReadableLogger.log("Finished downloading resync");
+            file.close();
+            fs.chmodSync(this.bin, 0o755);
+            this.downloading = false;
+            return resolve(DownloadState.Ok);
+          });
+
+          file.on("error", (e) => {
+            // vscode.window.showErrorMessage(
+            //   "Error writing file. Check the log for details"
+            // );
+            ReadableLogger.log("Error downloading resync");
+            ReadableLogger.log(e.message);
+            this.downloading = false;
+            return reject(DownloadState.Err);
+          });
+        });
+      } catch (err: any) {
+        // vscode.window.showErrorMessage(
+        //   "An error has occured while downloading resync"
+        // );
+        this.downloading = false;
+        ReadableLogger.log(err.toString());
+        return reject(DownloadState.Err);
+      }
+    });
+  }
+}
--- src/resync/executable.ts
+++ src/resync/executable.ts
@@ -5,6 +5,7 @@ import * as https from "https";
 import * as child_process from "child_process";
 import ReadableLogger from "../Logger";
 import { DownloadState } from "./types";
+import { DownloadManager } from "./downloadManager";
 
 /**
  * provides a wrapper around the resync executable
@@ -13,7 +14,6 @@ export default class Executable {
   private _onExecutableData: vscode.EventEmitter<string[]>;
   private dir: string;
   private bin: string;
-  private downloading: boolean = false;
   private process?: child_process.ChildProcessWithoutNullStreams;
 
   constructor(public readonly context: vscode.ExtensionContext) {
@@ -25,59 +25,6 @@ export default class Executable {
 
   public get onExecutableData(): vscode.Event<string[]> {
     return this._onExecutableData.event;
-  }
-
-  private async download(): Promise<DownloadState> {
-    if (this.isDownloaded() || this.downloading) {
-      return DownloadState.Ok;
-    }
-
-    return new Promise<DownloadState>((resolve, reject) => {
-      try {
-        ReadableLogger.log("Downloading resync");
-        this.downloading = true;
-        let platform = `resync_${process.platform}_${process.arch}`;
-        let downloadUrl = `https://resync.readable.workers.dev/${platform}`;
-
-        try {
-          fs.mkdirSync(this.dir);
-        } catch (err) {}
-
-        https.get(downloadUrl, { rejectUnauthorized: false }, (res) => {
-          let file = fs.createWriteStream(this.bin);
-          res.pipe(file);
-
-          file.on("finish", () => {
-            ReadableLogger.log("Finished downloading resync");
-            file.close();
-            fs.chmodSync(this.bin, 0o755);
-            this.downloading = false;
-            return resolve(DownloadState.Ok);
-          });
-
-          file.on("error", (e) => {
-            vscode.window.showErrorMessage(
-              "Error writing file. Check the log for details"
-            );
-            ReadableLogger.log("Error downloading resync");
-            ReadableLogger.log(e.message);
-            this.downloading = false;
-            return reject(DownloadState.Err);
-          });
-        });
-      } catch (err: any) {
-        vscode.window.showErrorMessage(
-          "An error has occured while downloading resync"
-        );
-        this.downloading = false;
-        ReadableLogger.log(err.toString());
-        return reject(DownloadState.Err);
-      }
-    });
-  }
-
-  public isDownloaded(): boolean {
-    return fs.existsSync(this.bin);
   }
 
   public async checkProject(folderPath: string) {
